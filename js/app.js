d876e5ba4645ca9071e1b9414860192a9eef00c6
Orson Bradford <orson@nosro.net>
2014-06-21 10:40:29 -0400

    Battle simulator initial commit. Angular framework, bootstrap.css, underscore, sortable js libraries. Functionality to Add/Remove/Sort units for each side of battle.

---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/* Main Game Module */
+
+/*
+var OpenWarUtilsModule = angular.module('openWarUtils', [])
+	.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/battle', {templateUrl: 'partials/battle.html', controller: OpenWarUtilsModule.BattleCtrl}).
+		when('/upgrade', {templateUrl: 'partials/upgrade.html', controller: OpenWarUtilsModule.UpgradeCtrl}).
+		otherwise({redirectTo: '/home'});
+	}])
+	.run(function( $rootScope) {
+	// listen for any change to the model, and show save/cancel buttons when model changes.
+
+	});
+*/
+var OpenWarUtilsApp = angular.module('OpenWarUtilsApp', []);
+
+OpenWarUtilsApp.factory('UnitTypesData', [function () {
+	var unitList = [
+ { id: '1', name: 'scout', attack: '0', defense: '0', cost: '10', upkeep: '1'}
+,{ id: '2', name: 'ranger', attack: '1', defense: '0', cost: '20', upkeep: '2'}
+,{ id: '3', name: 'lookout', attack: '0', defense: '1', cost: '20', upkeep: '2'}
+,{ id: '4', name: 'warrior', attack: '1', defense: '1', cost: '30', upkeep: '2'}
+,{ id: '5', name: 'archer', attack: '2', defense: '0', cost: '40', upkeep: '3'}
+,{ id: '6', name: 'guard', attack: '0', defense: '2', cost: '40', upkeep: '3'}
+,{ id: '7', name: 'knight', attack: '2', defense: '2', cost: '70', upkeep: '3'}
+,{ id: '8', name: 'fusileer', attack: '3', defense: '1', cost: '90', upkeep: '4'}
+,{ id: '9', name: 'sentinel', attack: '1', defense: '3', cost: '90', upkeep: '4'}
+,{ id: '10', name: 'champion', attack: '3', defense: '3', cost: '150', upkeep: '4'}
+,{ id: '11', name: 'autocannon', attack: '4', defense: '2', cost: '190', upkeep: '5'}
+,{ id: '12', name: 'shieldmech', attack: '2', defense: '4', cost: '190', upkeep: '5'}
+,{ id: '13', name: 'warstrider', attack: '4', defense: '4', cost: '310', upkeep: '5'}
+,{ id: '14', name: 'bomber', attack: '5', defense: '3', cost: '390', upkeep: '6'}
+,{ id: '15', name: 'tank', attack: '3', defense: '5', cost: '390', upkeep: '6'}
+,{ id: '16', name: 'juggernaut', attack: '5', defense: '5', cost: '630', upkeep: '6'}
+	];
+	return unitList
+}]);
---
+++ js/controllers.js
@@ -0,0 +1,135 @@
+'use strict';
+
+OpenWarUtilsApp.controller('BattleCtrl', ['$scope', function ($scope) {
+ 
+}]);
+
+OpenWarUtilsApp.controller('UnitCtrl', ['$scope', 'UnitTypesData', function ($scope, UnitTypesData) {
+	var shuffle = function (array) {
+		var m = array.length, t, i;
+
+		// While there remain elements to shuffle…
+		while (m) {
+
+		// Pick a remaining element…
+		i = Math.floor(Math.random() * m--);
+
+		// And swap it with the current element.
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+		}
+
+		return array;
+	},
+	// sort by indexes
+	// http://stackoverflow.com/q/4046967/3261678
+	getSorted = function(arr, sortArr) {
+		var result = [];
+		for(var i=0; i<arr.length; i++) {
+			result[i] = arr[sortArr[i]];
+		}
+		return result;
+	}
+	;
+	$scope.unitList = UnitTypesData;
+	$scope.units = [];
+	$scope.selectedUnit = null;
+	$scope.addUnit = function (unitType) {
+		var inc = 0,
+			newUnit = {
+				typeId : unitType.id,
+				inc : inc,
+				name : unitType.name,
+				attack : unitType.attack,
+				defense : unitType.defense,
+				cost : unitType.cost,
+				upkeep : unitType.upkeep,
+			},
+			unit
+		;
+
+		for (var i in $scope.units) {
+			unit = $scope.units[i];
+			if (unit.typeId == unitType.id) {
+				inc = Math.max(unit.inc, inc);
+			}
+		}
+		newUnit.inc = inc + 1;
+		newUnit.name = newUnit.name + '_' + newUnit.inc;
+		$scope.units.push(newUnit);
+		setupSortable();
+	};
+	$scope.removeUnit = function (unitName) {
+		var unit
+		;
+
+		for (var i in $scope.units) {
+			unit = $scope.units[i];
+			if (unit.name == unitName) {
+				break;
+			}
+		}
+
+		$scope.units.splice(i, 1);
+		setupSortable();
+	};
+	$scope.shuffleUnits = function () {
+		$scope.units = shuffle($scope.units);
+	};
+
+	console.log($scope.openWarUnits);
+
+	var setupSortable = function() {
+		var mySort = document.getElementById($scope.sortableId);
+		new Sortable(mySort, {
+			handle: '.glyphicon-sort',
+			draggable: 'tr',
+			onUpdate: function (evt){
+				// get new sort order based on indexes
+				var newSortIndexes = [];
+				var liElements = mySort.getElementsByTagName("tr");
+				for (var i=0; i<liElements.length; i++) {
+					newSortIndexes.push(liElements[i].getAttribute('data-index'));
+				}
+
+				// process change
+				$scope.newSortIndexes = newSortIndexes;
+				$scope.units = getSorted($scope.units, newSortIndexes);
+				$scope.$apply();
+				
+				// example of a standard $http request you would do
+				// note that you don't need $scope.$apply(); for $http requests
+				// http://jimhoskins.com/2012/12/17/angularjs-and-apply.html
+				/*
+				$http.post('your/url', { newSortIndexes: newSortIndexes }).success(function(data) {
+				$scope.people = data;
+				})
+				*/
+			}
+		});
+	}
+
+	//console.log($scope.attr('title'));
+}]);
+
+// OpenWarUtilsApp.directive('tooltip', [function () {
+// 	return {
+// 		restrict:'A',
+// 		link: function(scope, element, attrs)
+// 		{
+// 			$(element)
+// 				.attr('title',scope.$eval(attrs.tooltip))
+// 				.tooltip({placement: "right"});
+// 		}
+// 	}
+// }]);
+
+// OpenWarUtilsApp.directive('openWarUnits', [function(){
+// 	return {
+// 		restrict: 'A',
+// 		replace: false,
+// 		// transclude: true,
+// 		scope: { openWarUnits:'@' }
+// 	};
+// }]);
