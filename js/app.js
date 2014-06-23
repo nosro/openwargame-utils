3e63e4f2a6964791e3ee6062933719ccb4aabad6
Orson Bradford <orson@nosro.net>
2014-06-23 00:02:25 -0400

    Battle simulator. Set up a data service for the units, and begin the battle simulation function.

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@
 /* Main Game Module */
 
 /*
-var OpenWarUtilsModule = angular.module('openWarUtils', [])
+var openWarUtilsModule = angular.module('openWarUtils', [])
 	.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
-		when('/battle', {templateUrl: 'partials/battle.html', controller: OpenWarUtilsModule.BattleCtrl}).
-		when('/upgrade', {templateUrl: 'partials/upgrade.html', controller: OpenWarUtilsModule.UpgradeCtrl}).
+		when('/battle', {templateUrl: 'partials/battle.html', controller: openWarUtilsModule.BattleCtrl}).
+		when('/upgrade', {templateUrl: 'partials/upgrade.html', controller: openWarUtilsModule.UpgradeCtrl}).
 		otherwise({redirectTo: '/home'});
 	}])
 	.run(function( $rootScope) {
@@ -15,26 +15,116 @@ var OpenWarUtilsModule = angular.module('openWarUtils', [])
 
 	});
 */
-var OpenWarUtilsApp = angular.module('OpenWarUtilsApp', []);
+var openWarUtilsMod = angular.module('openWarUtilsMod', []);
 
-OpenWarUtilsApp.factory('UnitTypesData', [function () {
-	var unitTypeList = [
- { id: '1', name: 'scout', attack: '0', defense: '0', cost: '10', upkeep: '1'}
-,{ id: '2', name: 'ranger', attack: '1', defense: '0', cost: '20', upkeep: '2'}
-,{ id: '3', name: 'lookout', attack: '0', defense: '1', cost: '20', upkeep: '2'}
-,{ id: '4', name: 'warrior', attack: '1', defense: '1', cost: '30', upkeep: '2'}
-,{ id: '5', name: 'archer', attack: '2', defense: '0', cost: '40', upkeep: '3'}
-,{ id: '6', name: 'guard', attack: '0', defense: '2', cost: '40', upkeep: '3'}
-,{ id: '7', name: 'knight', attack: '2', defense: '2', cost: '70', upkeep: '3'}
-,{ id: '8', name: 'fusileer', attack: '3', defense: '1', cost: '90', upkeep: '4'}
-,{ id: '9', name: 'sentinel', attack: '1', defense: '3', cost: '90', upkeep: '4'}
-,{ id: '10', name: 'champion', attack: '3', defense: '3', cost: '150', upkeep: '4'}
-,{ id: '11', name: 'autocannon', attack: '4', defense: '2', cost: '190', upkeep: '5'}
-,{ id: '12', name: 'shieldmech', attack: '2', defense: '4', cost: '190', upkeep: '5'}
-,{ id: '13', name: 'warstrider', attack: '4', defense: '4', cost: '310', upkeep: '5'}
-,{ id: '14', name: 'bomber', attack: '5', defense: '3', cost: '390', upkeep: '6'}
-,{ id: '15', name: 'tank', attack: '3', defense: '5', cost: '390', upkeep: '6'}
-,{ id: '16', name: 'juggernaut', attack: '5', defense: '5', cost: '630', upkeep: '6'}
-	];
-	return unitTypeList
+openWarUtilsMod.value('unitTypes', [
+ { id: '1', name: 'scout', attack: 0, defense: 0, cost: 10, upkeep: 1}
+,{ id: '2', name: 'ranger', attack: 1, defense: 0, cost: 20, upkeep: 2}
+,{ id: '3', name: 'lookout', attack: 0, defense: 1, cost: 20, upkeep: 2}
+,{ id: '4', name: 'warrior', attack: 1, defense: 1, cost: 30, upkeep: 2}
+,{ id: '5', name: 'archer', attack: 2, defense: 0, cost: 40, upkeep: 3}
+,{ id: '6', name: 'guard', attack: 0, defense: 2, cost: 40, upkeep: 3}
+,{ id: '7', name: 'knight', attack: 2, defense: 2, cost: 70, upkeep: 3}
+,{ id: '8', name: 'fusileer', attack: 3, defense: 1, cost: 90, upkeep: 4}
+,{ id: '9', name: 'sentinel', attack: 1, defense: 3, cost: 90, upkeep: 4}
+,{ id: '10', name: 'champion', attack: 3, defense: 3, cost: 150, upkeep: 4}
+,{ id: '11', name: 'autocannon', attack: 4, defense: 2, cost: 190, upkeep: 5}
+,{ id: '12', name: 'shieldmech', attack: 2, defense: 4, cost: 190, upkeep: 5}
+,{ id: '13', name: 'warstrider', attack: 4, defense: 4, cost: 310, upkeep: 5}
+,{ id: '14', name: 'bomber', attack: 5, defense: 3, cost: 390, upkeep: 6}
+,{ id: '15', name: 'tank', attack: 3, defense: 5, cost: 390, upkeep: 6}
+,{ id: '16', name: 'juggernaut', attack: 5, defense: 5, cost: 630, upkeep: 6}
+]);
+
+// This can become a simple service factory if we don't need to configure it
+openWarUtilsMod.provider('forces', [function () {
+	var units = {};
+	this.units = {};
+
+	this.battles = 0;
+
+	// TODO - actually this is setClan
+	this.addClan = function (clanName) {
+		if (clanName) {
+			this.units[clanName] = [];
+		}
+		console.log('addClan', this.units);
+	}
+	// TODO - actually this is setUnits
+	this.addUnits = function (clanName, unitsCollection) {
+		if (clanName && unitsCollection) {
+			this.units[clanName] = unitsCollection;
+		}
+		console.log('addUnits', this.units);
+	}
+	this.getUnits = function (clanName) {
+
+		if (clanName) {
+			return this.units[clanName];
+		} else {
+			return this.units;
+		}
+		console.log('getUnits', this.units);
+	}
+
+// TODO this could be a separate battles service
+	this.updateBattles = function (n) {
+		this.battles = n;
+	}
+	this.getBattles = function () {
+		return this.battles;
+	}
+
+	this.$get = function () {
+		return {
+			'addClan' : this.addClan,
+			'addUnits' : this.addUnits,
+			'getUnits' : this.getUnits,
+			'addBattles' : this.addBattles,
+			'getBattles': this.getBattles,
+			'battles': this.battles,
+			'units' : this.units
+		}
+	}
 }]);
+
+openWarUtilsMod.config(function (forcesProvider) {
+	var knight = {
+			typeId : '7',
+			inc : 1,
+			name : 'knight_1',
+			attack : 2,
+			defense : 2,
+			cost : 7,
+			upkeep : 3
+		};
+	forcesProvider.addClan("The Good Guys");
+	knight.name = 'white_knight';
+	forcesProvider.addUnits("The Good Guys", new Array(angular.copy(knight)));
+	forcesProvider.addClan("The Bad Guys");
+	knight.name = 'black_knight';
+	forcesProvider.addUnits("The Bad Guys", new Array(angular.copy(knight)));
+});
+
+// openWarUtilsMod.directive('openWarClan', [function () {
+// 	return {
+// 		controller: function($scope, $element, $attrs) {
+// 			$scope.test = 'foo';
+// 			// clanCtrl.initClan(attrs.openWarClan);
+// 		}
+// 	}
+// }]);
+
+// openWarUtilsMod.directive('tooltip', [function () {
+// 	return {
+// 		restrict:'A',
+// 		link: function(scope, element, attrs)
+// 		{
+// 			$(element)
+// 				.attr('title',scope.$eval(attrs.tooltip))
+// 				.tooltip({placement: "right"});
+// 		}
+// 	}
+// }]);
+
+
--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,86 @@
 'use strict';
 
-OpenWarUtilsApp.controller('BattleCtrl', ['$scope', function ($scope) {
- 
+openWarUtilsMod.controller('BattleCtrl', ['$scope', 'forces', function ($scope, forces) {
+	$scope.fight = function () {
+		var clanForces = forces.getUnits(),
+			otherClanName,
+			units,
+			otherUnits,
+			unit,
+			otherUnit,
+			deal,
+			take,
+			damage
+		;
+
+		for (var clanName in clanForces) {
+			// While we just have two clans fighting this works TODO - pluralize for more
+			for (var testClanName in clanForces) {
+				if (testClanName != clanName) {
+					otherClanName = testClanName;
+					otherUnits = clanForces[otherClanName];
+				}
+			}
+			console.log(clanName + ' vs ' + otherClanName);
+			units = clanForces[clanName];
+			for (var i = 0; i < units.length; i++) {
+				unit = units[i];
+				if (!unit.dead) {
+					// TODO - this is not proper binary progression of value
+					unit.dealt = Math.floor(Math.random() * (unit.attack + 1));
+					console.log(clanName, unit.name, 'total dealt:', unit.dealt);
+					damage = 0;
+					for (var j = 0; j < otherUnits.length; j++) {
+						otherUnit = otherUnits[j];
+						if (otherUnit.dead || otherUnit.taken == otherUnit.blocked + 1) {
+							continue;
+						}
+						if (damage >= unit.dealt) {
+							break;
+						}
+						if (!otherUnit.blocked) {
+							// TODO - this is not proper binary progression of value
+							otherUnit.blocked = Math.floor(Math.random() * (otherUnit.defense + 1));
+						}
+						if (!otherUnit.taken) {
+							otherUnit.taken = 0;
+						}
+						console.log(clanName, unit.name, 'hits:', otherUnit.name, 'total blocked:', otherUnit.blocked);
+						// TODO - eventually we may have health
+						if (otherUnit.taken <= otherUnit.blocked + 1) {
+							deal = unit.dealt - damage;
+							take = Math.min(deal, otherUnit.blocked + 1 - otherUnit.taken);
+							otherUnit.taken = otherUnit.taken + take;
+							damage = damage + take;
+							console.log(otherClanName, otherUnit.name,'take/deal:', take + '/' + deal, 'total taken:', otherUnit.taken, 'damage dealt', damage);
+						}
+					}
+				}
+			}
+		};
+		for (var clanName in clanForces) {
+			console.log('cleanup', clanName);
+			units = clanForces[clanName];
+			// CLEANUP
+			for (var i = 0; i < units.length; i++) {
+				unit = units[i];
+				if (unit.taken >= unit.blocked + 1) {
+					console.log(unit, unit.taken, unit.blocked, ' dead')
+					unit.dead = 1;
+				}
+				unit.dealt = 0;
+				unit.blocked = 0;
+			}
+		}
+		console.log(forces.battles);
+		forces.battles = forces.battles + 1;
+		console.log(forces.battles);
+		$scope.error = "Fight!";
+		//$scope.error = "Sorry, can't fight - programming isn't done yet!";
+	}
 }]);
 
-OpenWarUtilsApp.controller('UnitCtrl', ['$scope', 'UnitTypesData', function ($scope, UnitTypesData) {
+openWarUtilsMod.controller('ClanCtrl', ['$scope', '$attrs', 'unitTypes', 'forces',  function ($scope, $attrs, unitTypes, forces) {
 	var shuffle = function (array) {
 		var m = array.length, t, i;
 
@@ -22,6 +98,14 @@ OpenWarUtilsApp.controller('UnitCtrl', ['$scope', 'UnitTypesData', function ($sc
 
 		return array;
 	},
+	safeName = function(name) {
+		return name.replace(/[^a-z0-9]/g, function(s) {
+			var c = s.charCodeAt(0);
+			if (c == 32) return '-';
+			if (c >= 65 && c <= 90) return s.toLowerCase();
+			return '__' + ('000' + c.toString(16)).slice(-4);
+		});
+	},
 	// sort by indexes
 	// http://stackoverflow.com/q/4046967/3261678
 	getSorted = function(arr, sortArr) {
@@ -30,12 +114,58 @@ OpenWarUtilsApp.controller('UnitCtrl', ['$scope', 'UnitTypesData', function ($sc
 			result[i] = arr[sortArr[i]];
 		}
 		return result;
+	},
+	setupSortable = function() {
+		var mySort = document.getElementById($scope.clanSortName),
+			group = $scope.clanName,
+			sortable = new Sortable(mySort, {
+			handle: '.sortable-handle',
+			draggable: '.sortable-item',
+			onUpdate: function (evt){
+				// get new sort order based on indexes
+				var newSortIndexes = [];
+				var itemsList = mySort.querySelectorAll('.sortable-item');
+				for (var i=0; i<itemsList.length; i++) {
+					newSortIndexes.push(itemsList[i].getAttribute('data-index'));
+				}
+
+				// process change
+				$scope.newSortIndexes = newSortIndexes;
+				$scope.units = getSorted($scope.units, newSortIndexes);
+				$scope.$apply();
+				
+				// example of a standard $http request you would do
+				// note that you don't need $scope.$apply(); for $http requests
+				// http://jimhoskins.com/2012/12/17/angularjs-and-apply.html
+				/*
+				$http.post('your/url', { newSortIndexes: newSortIndexes }).success(function(data) {
+				$scope.people = data;
+				})
+				*/
+			}
+		});
+	},
+	initClan = function(name) {
+		if (name) {
+			$scope.clanName = name.toString();
+			$scope.clanSortName = safeName(name.toString()) + '-sortable';
+			// TODO - could make a getClan method to test existence
+			if (forces.getUnits(name)) {
+				$scope.units = forces.getUnits(name);
+			} else {
+				forces.addClan(name);
+				$scope.units = forces.getUnits($scope.clanName);
+			}
+			updateUnits($scope.clanName, $scope.units);
+			$scope.battles = forces.battles;
+		}
+	},
+	updateUnits = function(name, units) {
+		forces.addUnits(name, units);
 	}
 	;
 
-	$scope.unitTypeList = UnitTypesData;
-	$scope.units = [];
-	$scope.selectedUnit = null;
+	// EXPORT METHODS FOR TEMPLATE UI BINDING
 	$scope.addUnit = function (unitType) {
 		var inc = 0,
 			newUnit = {
@@ -83,57 +213,16 @@ OpenWarUtilsApp.controller('UnitCtrl', ['$scope', 'UnitTypesData', function ($sc
 		$scope.units = shuffle($scope.units);
 	};
 
-	var setupSortable = function() {
-		var mySort = document.getElementById($scope.sortableId),
-			group = $scope.sortableId,
-			sortable = new Sortable(mySort, {
-			handle: '.sortable-handle',
-			draggable: '.sortable-item',
-			onUpdate: function (evt){
-				// get new sort order based on indexes
-				var newSortIndexes = [];
-				var liElements = mySort.getElementsByTagName("tr");
-				for (var i=0; i<liElements.length; i++) {
-					newSortIndexes.push(liElements[i].getAttribute('data-index'));
-				}
-
-				// process change
-				$scope.newSortIndexes = newSortIndexes;
-				$scope.units = getSorted($scope.units, newSortIndexes);
-				$scope.$apply();
-				
-				// example of a standard $http request you would do
-				// note that you don't need $scope.$apply(); for $http requests
-				// http://jimhoskins.com/2012/12/17/angularjs-and-apply.html
-				/*
-				$http.post('your/url', { newSortIndexes: newSortIndexes }).success(function(data) {
-				$scope.people = data;
-				})
-				*/
-			}
-		});
+	// INITIALIZE
+	$scope.unitTypeList = unitTypes;
+	$scope.selectedUnit = null;
+	if ('openWarClan' in $attrs) {
+		initClan($attrs.openWarClan);
 	}
 
-	//console.log($scope.attr('title'));
+	// TESTING
+	$scope.$watch('units', function (newValue, oldValue, scope) {
+		console.log('watcher')
+	});
+	//forces.addClan($scope.clanName);
 }]);
-
-// OpenWarUtilsApp.directive('tooltip', [function () {
-// 	return {
-// 		restrict:'A',
-// 		link: function(scope, element, attrs)
-// 		{
-// 			$(element)
-// 				.attr('title',scope.$eval(attrs.tooltip))
-// 				.tooltip({placement: "right"});
-// 		}
-// 	}
-// }]);
-
-// OpenWarUtilsApp.directive('openWarUnits', [function(){
-// 	return {
-// 		restrict: 'A',
-// 		replace: false,
-// 		// transclude: true,
-// 		scope: { openWarUnits:'@' }
-// 	};
-// }]);
